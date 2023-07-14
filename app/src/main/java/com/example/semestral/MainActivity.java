58f60e7dc02c380a28de2beebd6a14ba77a6e45a
Felipe0408 <105187424+Felipe0408@users.noreply.github.com>
2023-07-14 02:58:58 -0500

    Se hizo cambios a los .java

--- app/src/main/java/com/example/semestral/MainActivity.java
+++ app/src/main/java/com/example/semestral/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.semestral;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button BotonEmpezar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentacion);
+
+        BotonEmpezar = findViewById(R.id.bEmpezar);
+        BotonEmpezar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent para pasar del main a la pantalla de Login
+                Intent IntentToPreguntas = new Intent(MainActivity.this, Preguntas.class);
+                startActivity(IntentToPreguntas);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/semestral/Preguntas.java
+++ app/src/main/java/com/example/semestral/Preguntas.java
@@ -3,12 +3,176 @@ package com.example.semestral;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Preguntas extends AppCompatActivity {
+
+    RadioGroup Grupo1, Grupo2, Grupo3, Grupo4, Grupo5;
+    RadioButton P1_R1, P1_R2, P1_R3, P1_R4,
+            P2_R1, P2_R2, P2_R3, P2_R4,
+            P3_R1, P3_R2, P3_R3, P3_R4,
+            P4_R1, P4_R2, P4_R3, P4_R4,
+            P5_R1, P5_R2, P5_R3, P5_R4;
+
+    int Resp1 = 0, Resp2= 0, Resp3= 0, Resp4= 0, Resp5= 0, Total= 0;
+    int[] Arreglo = new int[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
+
+        //Se identifican los grupos
+        Grupo1 = findViewById(R.id.RG1);
+        Grupo2 = findViewById(R.id.RG2);
+        Grupo3 = findViewById(R.id.RG3);
+        Grupo4 = findViewById(R.id.RG4);
+        Grupo5 = findViewById(R.id.RG5);
+
+        //Se identifican los radio button
+        P1_R1 = findViewById(R.id.p1_r1);
+        P1_R2 = findViewById(R.id.p1_r2);
+        P1_R3 = findViewById(R.id.p1_r3);
+        P1_R4 = findViewById(R.id.p1_r4);
+        P2_R1 = findViewById(R.id.p2_r1);
+        P2_R2 = findViewById(R.id.p2_r2);
+        P2_R3 = findViewById(R.id.p2_r3);
+        P2_R4 = findViewById(R.id.p2_r4);
+        P3_R1 = findViewById(R.id.p3_r1);
+        P3_R2 = findViewById(R.id.p3_r2);
+        P3_R3 = findViewById(R.id.p3_r3);
+        P3_R4 = findViewById(R.id.p3_r4);
+        P4_R1 = findViewById(R.id.p4_r1);
+        P4_R2 = findViewById(R.id.p4_r2);
+        P4_R3 = findViewById(R.id.p4_r3);
+        P4_R4 = findViewById(R.id.p4_r4);
+        P5_R1 = findViewById(R.id.p5_r1);
+        P5_R2 = findViewById(R.id.p5_r2);
+        P5_R3 = findViewById(R.id.p5_r3);
+        P5_R4 = findViewById(R.id.p5_r4);
+
+        //Listener del grupo 1 de radio button
+        Grupo1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int selectedId) {
+                selectedId=Grupo1.getCheckedRadioButtonId();
+                RadioButton Respchoose = (RadioButton) findViewById(selectedId);
+                //Decide si sumar puntos y guarda la respuesta elegida en el arreglo en la posición 0
+                if (Respchoose == P1_R1){
+                    Resp1 = 1;
+                    Arreglo[0] = 1;
+                } else if (Respchoose == P1_R2){
+                    Resp1 = 0;
+                    Arreglo[0] = 2;
+                } else if (Respchoose == P1_R3){
+                    Resp1 = 0;
+                    Arreglo[0] = 3;
+                } else if (Respchoose == P1_R4){
+                    Resp1 = 0;
+                    Arreglo[0] = 4;
+                    //Toast Toast1 = Toast.makeText(getApplicationContext(), "Funciona"+Resp1+" "+Arreglo[0], Toast.LENGTH_LONG);
+                    //Toast1.setGravity(Gravity.CENTER, 200, -650);
+                    //Toast1.show();
+                }
+            }
+        });
+
+        //Listener del grupo 2 de radio button
+        Grupo2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int selectedId) {
+                selectedId=Grupo2.getCheckedRadioButtonId();
+                RadioButton Respchoose = (RadioButton) findViewById(selectedId);
+                //Decide si sumar puntos y guarda la respuesta elegida en el arreglo en la posición 0
+                if (Respchoose == P2_R1){
+                    Resp2 = 1;
+                    Arreglo[1] = 1;
+                } else if (Respchoose == P2_R2){
+                    Resp2 = 0;
+                    Arreglo[1] = 2;
+                } else if (Respchoose == P2_R3){
+                    Resp2 = 0;
+                    Arreglo[1] = 3;
+                } else if (Respchoose == P2_R4){
+                    Resp2 = 0;
+                    Arreglo[1] = 4;
+                }
+            }
+        });
+
+        //Listener del grupo 3 de radio button
+        Grupo3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int selectedId) {
+                selectedId=Grupo3.getCheckedRadioButtonId();
+                RadioButton Respchoose = (RadioButton) findViewById(selectedId);
+                //Decide si sumar puntos y guarda la respuesta elegida en el arreglo en la posición 0
+                if (Respchoose == P3_R1){
+                    Resp3 = 1;
+                    Arreglo[2] = 1;
+                } else if (Respchoose == P3_R2){
+                    Resp3 = 0;
+                    Arreglo[2] = 2;
+                } else if (Respchoose == P3_R3){
+                    Resp3 = 0;
+                    Arreglo[2] = 3;
+                } else if (Respchoose == P3_R4){
+                    Resp3 = 0;
+                    Arreglo[2] = 4;
+                }
+            }
+        });
+
+        //Listener del grupo 4 de radio button
+        Grupo4.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int selectedId) {
+                selectedId=Grupo4.getCheckedRadioButtonId();
+                RadioButton Respchoose = (RadioButton) findViewById(selectedId);
+                //Decide si sumar puntos y guarda la respuesta elegida en el arreglo en la posición 0
+                if (Respchoose == P4_R1){
+                    Resp4 = 1;
+                    Arreglo[3] = 1;
+                } else if (Respchoose == P4_R2){
+                    Resp4 = 0;
+                    Arreglo[3] = 2;
+                } else if (Respchoose == P4_R3){
+                    Resp4 = 0;
+                    Arreglo[3] = 3;
+                } else if (Respchoose == P4_R4){
+                    Resp4 = 0;
+                    Arreglo[3] = 4;
+                }
+            }
+        });
+
+        //Listener del grupo 4 de radio button
+        Grupo4.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int selectedId) {
+                selectedId=Grupo4.getCheckedRadioButtonId();
+                RadioButton Respchoose = (RadioButton) findViewById(selectedId);
+                //Decide si sumar puntos y guarda la respuesta elegida en el arreglo en la posición 0
+                if (Respchoose == P5_R1){
+                    Resp5 = 1;
+                    Arreglo[4] = 1;
+                } else if (Respchoose == P5_R2){
+                    Resp5 = 0;
+                    Arreglo[4] = 2;
+                } else if (Respchoose == P5_R3){
+                    Resp5 = 0;
+                    Arreglo[4] = 3;
+                } else if (Respchoose == P5_R4){
+                    Resp5 = 0;
+                    Arreglo[4] = 4;
+                }
+            }
+        });
+
+
+
     }
 }
