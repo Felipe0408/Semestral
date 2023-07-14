00835f8a4cb56492271fa079ef7e7fcae4a012f7
airlenys21 <105010041+airlenys21@users.noreply.github.com>
2023-07-14 00:57:32 -0500

    Creación de preguntas  -Correcciones
    Se realizó las correciones de los layout

--- app/src/main/java/com/example/semestral/resultados.java
+++ app/src/main/java/com/example/semestral/resultados.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class resultados extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_presentacion);
+        setContentView(R.layout.activity_resultados);
     }
 }
